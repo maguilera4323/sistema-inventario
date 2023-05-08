a65bff9bee9e3cf8ff127b320e09fbf4a679e3e5
maguilera4323 <maynoraguileraosorto@gmail.com>
2023-05-08 09:01:13 -0600

    aa

--- sistemas-master/config/app.php
+++
@@ -1,4 +0,0 @@
-<?php
-    const SERVERURL="http://localhost/sistemas/";  /* CUANDO SE TENGA EL URL DE HOSTING AQUI SE COLOCARA */
-    const COMPANY="Inversiones Sasuke";
-    date_default_timezone_set("America/Tegucigalpa"); /* configuracion de la zona horaria */
--- sistemas-master/vistas/contenidos/home-view.php
+++
@@ -1,9 +0,0 @@
-<?php
-//verifica si hay sesiones iniciadas
-if (session_status() !== PHP_SESSION_ACTIVE) {
-    session_start();
-}
-
-
-?>
-
